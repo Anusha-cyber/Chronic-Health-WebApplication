75f5eedf5ffa8315c98a45a20bbcd506ad30c5c2
Anusha Rajendran <ar652@student.le.ac.uk>
2023-12-10 21:15:02 +0530

    update the feedback

--- backend/src/main/java/com/smqa/smqa/appoinment/Appoinment.java
+++ backend/src/main/java/com/smqa/smqa/appoinment/Appoinment.java
@@ -6,8 +6,14 @@ public class RecordController {
 
     @PutMapping("/{recordId}/prescription-files")
     public ResponseEntity<Record> updatePrescriptionFiles(@PathVariable Long recordId,
-                                                         @RequestBody List<PrescriptionFile> newPrescriptionFiles) {
+            @RequestBody List<PrescriptionFile> newPrescriptionFiles) {
         Record updatedRecord = recordService.updatePrescriptionFiles(recordId, newPrescriptionFiles);
         return new ResponseEntity<>(updatedRecord, HttpStatus.OK);
+    }
+
+    @PostMapping("/feedback")
+    public ResponseEntity<Feedback> addFeedback(@RequestBody Feedback feedback) {
+        Feedback addedFeedback = feedbackService.addFeedback(feedback);
+        return new ResponseEntity<>(addedFeedback, HttpStatus.CREATED);
     }
 }
